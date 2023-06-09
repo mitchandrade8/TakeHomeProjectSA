d0ad98ecea6947b4192a9a547a396a29bad69ef1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-09 15:28:25 -0600

    -- break

--- TakeHomeProjectSA/SearchVC.swift
+++ TakeHomeProjectSA/SearchVC.swift
@@ -11,9 +11,20 @@ class SearchVC: UIViewController {
 
     let logoImageView = UIImageView()
     let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPurple
+        view.backgroundColor = .systemBackground
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+    }
+    
 }
