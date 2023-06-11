ce317c30cb35304753b76f568f471fb0350da265
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-11 05:47:22 -0600

    -- Start - Create Reusable Custom Alert

---
+++ TakeHomeProjectSA/GFTitleLabel.swift
@@ -0,0 +1,21 @@
+//
+//  GFTitleLabel.swift
+//  TakeHomeProjectSA
+//
+//  Created by Mitch Andrade on 6/11/23.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
