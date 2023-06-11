27ebb3b4fd563c50e9016e07a57dda05a182c97c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-11 06:02:08 -0600

    -- Error fixes

--- TakeHomeProjectSA/GFAlertVC.swift
+++ TakeHomeProjectSA/GFAlertVC.swift
@@ -19,9 +19,17 @@ class GFAlertVC: UIViewController {
     var buttonTitle: String?
     
     init(title: String, message: String, buttonTitle: String) {
-        
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
