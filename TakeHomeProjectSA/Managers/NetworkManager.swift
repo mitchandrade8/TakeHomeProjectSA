50a8c920d7e3e9739372a079939efa58bd687e2e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-12 12:50:59 -0600

    -- Start - Refactor Error Message

--- TakeHomeProjectSA/Managers/NetworkManager.swift
+++ TakeHomeProjectSA/Managers/NetworkManager.swift
@@ -44,7 +44,8 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "The data received from the server was invalid. Please try again.")
+                completed(nil, error.localizedDescription)
+//                completed(nil, "The data received from the server was invalid. Please try again.")
             }
         }
         
---
+++ TakeHomeProjectSA/Utilities/ErrorMessage.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorMessage.swift
+//  TakeHomeProjectSA
+//
+//  Created by Mitch Andrade on 6/12/23.
+//
+
+import Foundation
+
