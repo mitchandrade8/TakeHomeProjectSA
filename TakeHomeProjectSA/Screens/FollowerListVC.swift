204fbd6b3dd84c5877b9cff5df86a1110dc4f6fd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-13 06:43:39 -0600

    -- Finished - UICollectionView - FlowLayout

--- TakeHomeProjectSA/Screens/FollowerListVC.swift
+++ TakeHomeProjectSA/Screens/FollowerListVC.swift
@@ -41,8 +41,17 @@ class FollowerListVC: UIViewController {
     
     
     func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
         
-        return UICollectionViewFlowLayout()
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout()
     }
     
     
